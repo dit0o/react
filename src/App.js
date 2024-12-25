fcd50d34900e8a47517a3b47c4d75cf7ed518352
dit0o <gaganam6985@gmail.com>
2024-12-26 00:27:12 +0530

    useref 1

--- src/App.js
+++ src/App.js
@@ -1,20 +1,21 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import './App.css';
 
 
 
 function App() {
-  const [count,setCount]=useState(0)
-  
+  const [value,setValue]=useState(0)
+  const count=useRef(0)
   useEffect(()=>{
-    setTimeout(()=>{
-      setCount(count=>count+1)
-   
-    },2000)
-  },[count])
+    count.current=count.current+1;
+  });
+ 
   return (
     <>
-      <h1>I ve rendered {count} times!</h1>
+     <button onClick={()=>{setValue(prev=>prev-1)}}>-1</button>
+     <h1>{value}</h1>
+     <button onClick={()=>{setValue(prev=>prev+1)}}>+1</button>
+     <h1>Render Count:{count.current}</h1>
     </>
   );
 }
