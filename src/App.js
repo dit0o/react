bca2ee6c88dea0fedfc6189ad038741c275e8870
dit0o <gaganam6985@gmail.com>
2024-12-26 18:02:38 +0530

    useReducer2

--- src/App.js
+++ src/App.js
@@ -1,37 +1,42 @@
-import React, {  useReducer, useState } from 'react'
+import React, { useReducer, useState } from 'react'
 import './App.css';
 
 
 
 function App() {
-  const initialState={count:0}
-  const reducer=(state,action)=>{
-    switch(action.type){
+  const initialState = { count: 0 }
+  const reducer = (state, action) => {
+    switch (action.type) {
       case 'increase': {
-        return {count:state.count+1}
+        return { count: state.count + 1 }
       }
-case 'decrease':{
-  return {count:state.count-1}
-}
-default:{
-  return state
-}
+      case 'decrease': {
+        return { count: state.count - 1 }
+      }
+      case 'input': {
+        return { count: action.payload }
+      }
+      default: {
+        return state
+      }
     }
-   
+
   }
-  const[state,dispatch]=useReducer(reducer,initialState)
-  
+  const [state, dispatch] = useReducer(reducer, initialState)
+
 
 
   return (
     <>
       <h1>{state.count}</h1>
       <button onClick={() => {
-        dispatch({type:'increase'})
+        dispatch({ type: 'increase' })
       }}>Increase</button>
       <button onClick={() => {
-       dispatch({type:'decrease'})
+        dispatch({ type: 'decrease' })
       }}>Decrease</button>
+      <input value={state.count} onChange={(e) => dispatch({ type: 'input', payload: Number(e.target.value) })} type="number" />
+      <br />
     </>
   );
 }
