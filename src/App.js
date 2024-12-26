3a868637d247b5af5d0dec66b132306976790289
dit0o <gaganam6985@gmail.com>
2024-12-26 12:24:40 +0530

    UseMemo

--- src/App.js
+++ src/App.js
@@ -1,19 +1,25 @@
-import React, { useEffect, useRef, useState } from 'react'
+import React, { useEffect, useMemo, useRef, useState } from 'react'
 import './App.css';
 
 
 
 function App() {
- const inputElement=useRef();
-const btnclicked=()=>{
-  
-  inputElement.current.style.background="red"
-}
- 
+  const [number, setNumber] = useState(0)
+
+  const [counter, setCounter] = useState(0)
+  function cubeNum(num) {
+    console.log("calculation done")
+    return Math.pow(num, 3)
+  }
+  const result = useMemo(()=>{return cubeNum(number)},[number])
   return (
     <>
-    <input type='text' ref={inputElement}/>
-    <button onClick={btnclicked}>click here</button>
+      <input type='number' value={number} onChange={(e) => { setNumber(e.target.value) }} />
+      <h1>Cube Of the number:{result}</h1>
+      <button onClick={() => {
+        setCounter(counter + 1)
+      }}>countet++</button>
+      <h1>Counte:{counter}</h1>
     </>
   );
 }
