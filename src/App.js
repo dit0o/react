d8c0b7fbfb7018ccf4c759823293f59d7dbb445c
dit0o <gaganam6985@gmail.com>
2024-12-26 18:23:49 +0530

    useLayout

--- src/App.js
+++ src/App.js
@@ -1,42 +1,23 @@
-import React, { useReducer, useState } from 'react'
+import React, { useEffect, useLayoutEffect } from 'react'
 import './App.css';
 
 
 
 function App() {
-  const initialState = { count: 0 }
-  const reducer = (state, action) => {
-    switch (action.type) {
-      case 'increase': {
-        return { count: state.count + 1 }
-      }
-      case 'decrease': {
-        return { count: state.count - 1 }
-      }
-      case 'input': {
-        return { count: action.payload }
-      }
-      default: {
-        return state
-      }
-    }
-
-  }
-  const [state, dispatch] = useReducer(reducer, initialState)
-
-
-
+  useEffect(()=>{
+    console.log("useEffect")
+  },[])
+  useLayoutEffect(()=>{
+    console.log("useLayoutEffect")
+  },[])
+ 
   return (
     <>
-      <h1>{state.count}</h1>
-      <button onClick={() => {
-        dispatch({ type: 'increase' })
-      }}>Increase</button>
-      <button onClick={() => {
-        dispatch({ type: 'decrease' })
-      }}>Decrease</button>
-      <input value={state.count} onChange={(e) => dispatch({ type: 'input', payload: Number(e.target.value) })} type="number" />
-      <br />
+    <h2>Test message</h2>
+    {Array(400).fill('').map((item,index)=>(
+<li key={index}>
+  {Math.pow(Math.random,10)}</li>
+    ))}
     </>
   );
 }
