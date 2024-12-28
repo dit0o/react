198fb4d79a63a77a144a7b0d363b244ccf27b159
dit0o <gaganam6985@gmail.com>
2024-12-28 18:10:11 +0530

    review

--- src/List.js
+++ src/List.js
@@ -1,20 +1,60 @@
-import React from 'react';
-
-const List=({people})=>{
-
+import React, { useState } from 'react';
+import { FaChevronLeft, FaChevronRight, FaQuoteRight } from 'react-icons/fa'
+import people from './data'
+const List=()=>{
+const[index,setIndex]=useState(0)
+const{id,name,job,image,text}=people[index]
+const checkNumber=((number)=>{
+if(number>people.length-1){
+    return 0;
+}
+if(number<0){
+    return people.length-1
+}
+return number
+})
+const nextPerson = () => {
+    setIndex((index) => {
+      let newIndex = index + 1;
+      return checkNumber(newIndex);
+    });
+  };
+  const prevPerson = () => {
+    setIndex((index) => {
+      let newIndex = index - 1;
+      return checkNumber(newIndex);
+    });
+  };
+  const randomPerson = () => {
+    let randomNumber = Math.floor(Math.random() * people.length);
+    if (randomNumber === index) {
+      randomNumber = index + 1;
+    }
+    setIndex(checkNumber(randomNumber));
+  };
     return(
         <>
-{people.map((person)=>{
-const{id,name,age,image}=person;
-return(
-<article key={id}>
-    <img src={image} alt={name}/>
-    <h1>{name}</h1>
-    <p>{age} years</p>
-</article>
-)
+ <div className='img-container'>
+        <img src={image} alt={name} className='person-img' />
+        <span className='quote-icon'>
+          <FaQuoteRight />
+        </span>
+      </div>
+      <h4 className='author'>{name}</h4>
+      <p className='job'>{job}</p>
+      <p className='info'>{text}</p>
+      <div className='button-container'>
+        <button className='prev-btn' onClick={prevPerson}>
+          <FaChevronLeft />
+        </button>
+        <button className='next-btn' onClick={nextPerson}>
+          <FaChevronRight />
+        </button>
+      </div>
+      <button className='random-btn' onClick={randomPerson}>
+        surprise me
+      </button>
 
-})}
         </>
     )
   
