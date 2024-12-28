e6701d73ed152a0a5df09869a0d4f7c48779d653
dit0o <gaganam6985@gmail.com>
2024-12-28 16:17:53 +0530

    birthdaylist

--- src/List.js
+++ src/List.js
@@ -1,22 +1,23 @@
 import React from 'react';
 
-const List = ({ people }) => {
-  return (
-    <>
-      {people.map((person) => {
-        const { id, name, age, image } = person;
-        return (
-          <article key={id} className='person'>
-            <img src={image} alt={name} />
-            <div>
-              <h4>{name}</h4>
-              <p>{age} years</p>
-            </div>
-          </article>
-        );
-      })}
-    </>
-  );
-};
+const List=({people})=>{
+
+    return(
+        <>
+{people.map((person)=>{
+const{id,name,age,image}=person;
+return(
+<article key={id}>
+    <img src={image} alt={name}/>
+    <h1>{name}</h1>
+    <p>{age} years</p>
+</article>
+)
+
+})}
+        </>
+    )
+  
+}
 
 export default List;
