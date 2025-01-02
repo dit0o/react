07d8d253aa19505e84c40838d87997c8175ccc63
dit0o <gaganam6985@gmail.com>
2025-01-02 12:52:11 +0530

    Weather app

--- src/App.js
+++ src/App.js
@@ -1,28 +1,16 @@
 import React from 'react'
-import './App.css';
-import useLocalStorage from './hooks/useLocalStorage';
+import Weather from './components/Weather';
+
+
 
 
 
 function App() {
-  const[name,setName]=useLocalStorage('username','')
-  const[Id,setId]=useLocalStorage('useid','')
-// const[name,setName]=useState(
-//   localStorage.getItem('username')?
-//   localStorage.getItem('username'):''
-// )
-//  useEffect(()=>{
-//   localStorage.setItem('username',name)
-//  },[name])
-  return (
-    <>
-   <input type='text' placeholder='Enter yout name' value={name} onChange={(e)=>setName(e.target.value)}/>
-   <h2>Hello,{name}!</h2>
-   <input type='text' placeholder='Enter yout id' value={Id} onChange={(e)=>setId(e.target.value)}/>
-   <h2>Hello,{Id}!</h2>
 
-   
-    </>
+  return (
+  <div className='app'>
+<Weather/>
+  </div>
   );
 }
 
